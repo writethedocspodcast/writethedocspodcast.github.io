f73e0486ef34567945383766aa5efa6182da9d17
Tom Johnson <tomjoht@gmail.com>
2018-10-22 11:44:11 -0700

    make js and css assets local in order to use https

---
+++ assets/js/readthedocs-data.js
@@ -0,0 +1,19 @@
+var READTHEDOCS_DATA = {
+    project: "writethedocs-www",
+    version: "latest",
+    language: "en",
+    programming_language: "",
+    subprojects: {},
+    canonical_url: "https://www.writethedocs.org/",
+    theme: "alabaster",
+    builder: "sphinx",
+    docroot: "/docs/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "27fb52f0",
+    ad_free: true,
+
+    global_analytics_code: 'UA-17997319-1',
+    user_analytics_code: 'UA-37795838-1'
+};
+
